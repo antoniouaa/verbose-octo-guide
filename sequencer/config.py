7da293935355c78700028fece426c6564a7590b1
antoniouaa <antoniouaa@hotmail.com>
2021-02-17 19:52:32 +0000

    attempt at introducing CI tests

--- sequencer/config.py
+++ sequencer/config.py
@@ -6,7 +6,10 @@ class Config:
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv("DATABASE_URL")
+        or "postgres://postgres:postgres@localhost:5432/genome_sequencer"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
