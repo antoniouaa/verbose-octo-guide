b7bd7d8b1070a670d91e970ee4b310dd0ee7768c
antoniouaa <antoniouaa@hotmail.com>
2021-02-18 22:03:41 +0000

    hash issue maybe solved

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import dotenv
 import os
-from sqlalchemy.exc import IntegrityError
 
 dotenv.load_dotenv()
 
@@ -9,10 +8,11 @@ from sequencer.user import models
 from sequencer.config import ProductionConfig, DevelopmentConfig
 
 config = DevelopmentConfig if os.getenv("DEV_ENV_FLAG") else ProductionConfig
-app = create_app()
+app = create_app(config)
 with app.app_context():
     db.create_all()
     username = os.getenv("ROOT_USERNAME")
     password = os.getenv("ROOT_PASSWORD")
+    print(models.User.query.filter_by(username=username).first())
     if models.User.query.filter_by(username=username).first() is None:
         models.create_user(username=username, password=password)
--- sequencer/user/models.py
+++ sequencer/user/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(), nullable=False, unique=True)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(), nullable=False)
     created_on = db.Column(db.DateTime, default=datetime.utcnow)
     updated_on = db.Column(db.DateTime, default=datetime.utcnow)
 
